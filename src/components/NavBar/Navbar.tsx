639db48f6a6be73bf6a75a7a7e76b8cbc3a9e4c7
Avi Iskaov <avitech1242@gmail.com>
2023-02-02 17:52:34 +0200

    Add initial routes

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -1,13 +1,17 @@
 import { FunctionComponent } from 'react'
 import './Navbar.css'
 import img from '../../assets/images/logo.svg'
+import { useNavigate } from 'react-router-dom'
 
 export const Navbar: FunctionComponent = () => {
+  const navigate = useNavigate()
   return (
-    <div className="top-nav ps-3 text-white fixed-top">
-      <div className="row align-items-center">
+    <div className="top-nav ps-3 text-white fixed-top ">
+      <div className="row align-items-center pb-2">
         <div className="d-inline col-3 col-lg-1 logo p-4">
-          <img src={img} alt="" width={150} />
+          <button className="bg-transparent border-0" onClick={() => navigate('/')}>
+            <img src={img} alt="" width={150} />
+          </button>
         </div>
         <nav className="col-9 col-lg-11">
           <ul className="d-flex gap-4 m-0 w-100">
--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -1,9 +1,11 @@
 import { FunctionComponent } from 'react'
 
 import './Services.css'
-import { GenerationCards } from '../../consts/generation-types'
+import { GenerationCards } from '../../shared/consts/generation-types'
+import { useNavigate } from 'react-router-dom'
 
 export const Services: FunctionComponent = () => {
+  const navigate = useNavigate()
   const lines = ['Thumbnails for youtube', 'Post for new single', 'Text for ads', '15 second video out of your song']
 
   return (
@@ -15,25 +17,29 @@ export const Services: FunctionComponent = () => {
             <div className="d-inline ">Generate</div>
             <div className="multi-line-container rounded">
               <div className="lines text-start">
-                {lines.map((line) => (
-                  <div className="line">{line}</div>
+                {lines.map((line, idx) => (
+                  <div className="line" key={idx}>
+                    {line}
+                  </div>
                 ))}
               </div>
             </div>
           </div>
         </div>
         <div className="row mb-5 text-start">
-          {GenerationCards.map(({ icon, text }) => (
-            <div className="col-4 p-2 pointer">
-              <div className="bg-white h-100 rounded-md border border-2">
-                <div className="d-flex gap-4 align-items-center ps-2">
-                  <div className="icon p-4 rounded my-2">
-                    <img src={icon} alt="" width={60} height={60} />
+          {GenerationCards.map(({ icon, text, type }, idx) => (
+            <button className="col-4 p-2 pointer border-0" role="button" onClick={() => navigate(`/generator/${type}`)}>
+              <div key={idx}>
+                <div className="bg-white h-100 rounded-md border border-2">
+                  <div className="d-flex gap-4 align-items-center ps-2">
+                    <div className="icon p-4 rounded my-2">
+                      <img src={icon} alt="" width={60} height={60} />
+                    </div>
+                    <div className="pe-1">{text}</div>
                   </div>
-                  <div className="pe-1">{text}</div>
                 </div>
               </div>
-            </div>
+            </button>
           ))}
         </div>
         <button className="border border-dark text-black bg-transparent px-4 py-3 rounded-1">Contact us for more information</button>
