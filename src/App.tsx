4988469e9e047f58ba73ffaa8ff80a386721d72e
Avi Iskaov <avitech1242@gmail.com>
2023-02-02 17:52:34 +0200

    Add initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { BrowserRouter, Routes, Route, Link, useLocation, useNavigate } from 'react-router-dom'
 import { Navbar } from './components/NavBar/Navbar'
+import { Generator } from './pages/Generator/Generator'
 import { Home } from './pages/Home/Home'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Home />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/generator" element={<Generator />} />
+        <Route path="/generator/:type" element={<Generator />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
