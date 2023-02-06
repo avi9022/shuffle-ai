52cfd1a54033b6e9404379301b7fc678e9399c2e
tal <talekroni01@gmail.com>
2023-02-06 09:52:18 +0200

    Add crud buttons to genereted content

--- src/components/GeneratedContent/DynamicContent.tsx
+++ src/components/GeneratedContent/DynamicContent.tsx
@@ -3,10 +3,19 @@ import { useParams } from "react-router-dom";
 import { generationType } from "../../types/generation";
 import { GeneratedPostContent } from "./GeneratedPostContent";
 import { GeneratedVideoContent } from "./GeneratedVideoContent";
-
+import { BsHeart } from "react-icons/bs";
+import { FiEdit2 } from "react-icons/fi";
+import { RiFileCopyLine } from "react-icons/ri";
+import { useRecoilValue } from "recoil";
+import {
+  currentUserGenerationAtom,
+  currentUserVideoGenerationAtom,
+} from "../../state/atoms/userGenerations";
 
 export const DynamicContent: FunctionComponent = () => {
   const { type } = useParams();
+  const userGeneration = useRecoilValue(currentUserGenerationAtom);
+  const userVideoGeneration = useRecoilValue(currentUserVideoGenerationAtom);
   const getContent = () => {
     switch (type) {
       case generationType.post:
@@ -15,11 +24,11 @@ export const DynamicContent: FunctionComponent = () => {
       case generationType.video:
       case generationType.video916:
         return <GeneratedVideoContent />;
-    //   case generationType.youtube:
-    //   case generationType.tiktok:
-    //     return <ThumbnailForm />;
-    //   case generationType.cover:
-    //     return <CoverForm />;
+      //   case generationType.youtube:
+      //   case generationType.tiktok:
+      //     return <ThumbnailForm />;
+      //   case generationType.cover:
+      //     return <CoverForm />;
       default:
         return <>Choose what to generate</>;
     }
@@ -28,6 +37,22 @@ export const DynamicContent: FunctionComponent = () => {
   return (
     <div className="d-flex flex-column justify-content-between h-100 p-4">
       {getContent()}
+      {userGeneration && (
+        <div className="w-100 d-flex gap-2 justify-content-center">
+          <button className="btn btn-secondary d-flex gap-1 align-items-center">
+            <BsHeart />
+            Share
+          </button>
+          <button className="btn btn-secondary d-flex gap-1 align-items-center">
+            <RiFileCopyLine />
+            Copy text
+          </button>
+          <button className="btn btn-secondary d-flex gap-1 align-items-center">
+            <FiEdit2 />
+            Edit text
+          </button>
+        </div>
+      )}
     </div>
   );
 };
