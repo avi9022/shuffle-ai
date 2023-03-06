46592ee3c9c25ac1195d222f7a189ce57af8271c
Avi Iskaov <avitech1242@gmail.com>
2023-03-06 18:03:47 +0200

    Add styling to reusable components

--- src/components/Input/TextAreaInput.tsx
+++ src/components/Input/TextAreaInput.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react'
+import './Input.css'
 
 interface InputTextProps extends React.InputHTMLAttributes<HTMLTextAreaElement> {
   label?: string
@@ -6,13 +7,14 @@ interface InputTextProps extends React.InputHTMLAttributes<HTMLTextAreaElement> 
   preventDBlock?: boolean
   inputClassName?: string
   rows?: number
+  inputType?: 'primary'
 }
 
-export const TextAreaInput: FunctionComponent<InputTextProps> = ({ id, className, children, label, labelClassName, preventDBlock, inputClassName, rows, ...props }) => {
+export const TextAreaInput: FunctionComponent<InputTextProps> = ({ id, className, children, label, labelClassName, preventDBlock, inputClassName, inputType = 'primary', rows, ...props }) => {
   return (
     <label className={`mb-3 ${preventDBlock ? '' : 'd-block'} ${className}`}>
       <p className={`mb-1 ${labelClassName}`}>{label}</p>
-      <textarea rows={rows || 5} className={`form-control resize-none outline-0 text-truncate placeholder-gray ${inputClassName}`} {...props}>
+      <textarea rows={rows || 5} className={`input-${inputType} resize-none outline-0 text-truncate placeholder-gray ${inputClassName}`} {...props}>
         {children}
       </textarea>
     </label>
--- src/components/Input/TextInput.tsx
+++ src/components/Input/TextInput.tsx
@@ -1,17 +1,19 @@
 import React, { FunctionComponent } from 'react'
+import './Input.css'
 
 interface InputTextProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string
   labelClassName?: string
   preventDBlock?: boolean
   inputClassName?: string
+  inputType?: 'primary'
 }
 
-export const TextInput: FunctionComponent<InputTextProps> = ({ id, className, children, label, labelClassName, preventDBlock, inputClassName, ...props }) => {
+export const TextInput: FunctionComponent<InputTextProps> = ({ id, className, children, label, labelClassName, preventDBlock, inputClassName, inputType = 'primary', ...props }) => {
   return (
     <label className={`mb-3 ${preventDBlock ? '' : 'd-block'} ${className}`}>
       {label && <p className={`mb-1 ${labelClassName}`}>{label}</p>}
-      <input type="text" className={`form-control outline-0 text-truncate placeholder-gray ${inputClassName}`} {...props}>
+      <input type="text" className={`input-${inputType} outline-0 text-truncate placeholder-gray ${inputClassName}`} {...props}>
         {children}
       </input>
     </label>
