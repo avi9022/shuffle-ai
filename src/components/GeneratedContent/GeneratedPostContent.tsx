4a32d90662f1ed82a2b300e15bb2bd93fba2b7d2
tal <talekroni01@gmail.com>
2023-03-10 09:27:03 +0200

    Update logic

--- src/components/GeneratedContent/GeneratedPostContent.tsx
+++ src/components/GeneratedContent/GeneratedPostContent.tsx
@@ -1,47 +1,40 @@
-import { FunctionComponent } from 'react'
-import { useRecoilValue } from 'recoil'
-import { useState, useEffect } from 'react'
-import contentPlaceholder from '../../assets/images/generation-placeholder.svg'
-import { isSetUserGenerationsAtom, userPostGenerationsAtom } from '../../state/atoms/userGenerations'
-import loading from '../../assets/images/loading.svg'
+import { FunctionComponent } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import contentPlaceholder from "../../assets/images/generation-placeholder.svg";
+import {
+  isSetUserGenerationsAtom,
+  userPostGenerationsAtom,
+} from "../../state/atoms/userGenerations";
+import loading from "../../assets/images/loading.svg";
+import { TextEditor } from "../TextEditor";
 
 export const GeneratedPostContent: FunctionComponent = () => {
-  const userGeneration = useRecoilValue(userPostGenerationsAtom)
-  const isLoading = useRecoilValue(isSetUserGenerationsAtom)
-  const [text, setText] = useState('')
-  const [fullText, setFullText] = useState<string | ''>('Every day im shuffeling and you know it')
-  const [index, setIndex] = useState(0)
+  const [userGeneration, setUserGeneration] = useRecoilState(
+    userPostGenerationsAtom
+  );
 
-  useEffect(() => {
-    setIndex(0)
-    setText('')
-    if (userGeneration?.text) setFullText(userGeneration?.text)
-  }, [userGeneration?.text])
+  const isLoading = useRecoilValue(isSetUserGenerationsAtom);
+  if (isLoading) return <img src={loading} alt="loading" />;
 
-  useEffect(() => {
-    if (index < fullText.length) {
-      setTimeout(() => {
-        setText(text + fullText[index])
-        setIndex(index + 1)
-      }, 40)
-    }
-  }, [index])
-  if (isLoading)
-    return (
-      <div className="d-flex justify-content-center align-items-center h-100">
-        <img src={loading} alt="" />
-      </div>
-    )
   return (
     <div className="d-flex">
-      {!!userGeneration ? (
-        <div>{userGeneration.text}</div>
+      {!!userGeneration?.text ? (
+        <div>
+          <TextEditor
+            content={userGeneration.text}
+            onSetValue={(text: string) => {
+              if (text !== "") {
+                setUserGeneration({ text });
+              }
+            }}
+          />
+        </div>
       ) : (
-        <div className=" w-100">
-          <img className="" src={contentPlaceholder} alt="" width={150} />
-          <div className="w-100 align-self-start">{text}</div>
+        <div className="w-100 d-flex flex-column justify-content-center align-items-center">
+          <img src={contentPlaceholder} alt="" width={150} />
+          <div>Every day im shuffeling and you know it</div>
         </div>
       )}
     </div>
-  )
-}
+  );
+};
